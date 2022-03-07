12d2f1440fee6c7b0c62a606d6b1c1f8de39064d
Filip Spychala <filipspych@gmail.com>
2022-03-07 09:37:25 +0100

    init commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+       assertEquals(11, new Calculator().add(5, 6));
+    }
+}
